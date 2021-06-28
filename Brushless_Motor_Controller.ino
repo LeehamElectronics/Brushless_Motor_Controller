f59f2dab10486af7a64bcb66cf21714696184cf9
liamd <liamdanielprice@gmail.com>
2021-06-28 16:00:23 +1000

    Add project files.

---
+++ Brushless_Motor_Controller.ino
@@ -0,0 +1,32 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       Brushless_Motor_Controller.ino
+    Created:	28/06/2021 3:05:00 PM
+    Author:     LIAMS-MAIN-DESK\liamd
+*/
+
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+
+
+}
