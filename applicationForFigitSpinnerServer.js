fbc5075841ef3efc8c9916ec133bb0687d9b3a7c
liamd <liamdanielprice@gmail.com>
2021-06-28 18:15:46 +1000

    added web docs

---
+++ applicationForFigitSpinnerServer.js
@@ -0,0 +1,64 @@
+﻿/*
+██╗░░░░░███████╗███████╗██╗░░██╗░█████╗░███╗░░░███╗██╗░██████╗      ░██████╗░█████╗░███████╗████████╗░██╗░░░░░░░██╗░█████╗░██████╗░███████╗
+██║░░░░░██╔════╝██╔════╝██║░░██║██╔══██╗████╗░████║╚█║██╔════╝      ██╔════╝██╔══██╗██╔════╝╚══██╔══╝░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
+██║░░░░░█████╗░░█████╗░░███████║███████║██╔████╔██║░╚╝╚█████╗░      ╚█████╗░██║░░██║█████╗░░░░░██║░░░░╚██╗████╗██╔╝███████║██████╔╝█████╗░░
+██║░░░░░██╔══╝░░██╔══╝░░██╔══██║██╔══██║██║╚██╔╝██║░░░░╚═══██╗      ░╚═══██╗██║░░██║██╔══╝░░░░░██║░░░░░████╔═████║░██╔══██║██╔══██╗██╔══╝░░
+███████╗███████╗███████╗██║░░██║██║░░██║██║░╚═╝░██║░░░██████╔╝      ██████╔╝╚█████╔╝██║░░░░░░░░██║░░░░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║███████╗
+╚══════╝╚══════╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░░╚═════╝░     ╚═════╝░░╚════╝░╚═╝░░░░░░░░╚═╝░░░░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝
+*/
+
+var config = {
+ apiKey: "REDACTED",
+    authDomain: "REDACTED",
+    databaseURL: "REDACTED",
+    projectId: "REDACTED",
+    storageBucket: "REDACTED",
+    messagingSenderId: "REDACTED",
+    appId: "REDACTED"
+
+};
+
+// Initialize your Firebase app
+firebase.initializeApp(config);
+const preObject = document.getElementById('object');
+var movement = firebase.database().ref().child('object');
+var maxSpeed = firebase.database().ref().child('maxSpeed');
+var maxAcceleration = firebase.database().ref().child('maxAcceleration');
+var FS = firebase.database().ref("FS");
+movement.on('value', snap => console.log(snap.val()));
+maxSpeed.on('value', snap => console.log(snap.val()));
+maxAcceleration.on('value', snap => console.log(snap.val()));
+
+// Reference to the recommendations object in your Firebase database
+//var movement = firebase.database().ref("automatedCabinet");
+
+/* Save a new recommendation to the database, using the input in the form
+var submitRecommendation = function () {
+
+  // Get input values from each of the form elements
+  var title = $("#talkTitle").val();
+  var presenter = $("#talkPresenter").val();
+  var link = $("#talkLink").val();
+
+  // Push a new recommendation to the database using those values
+  recommendations.push({
+    "title": title,
+    "presenter": presenter,
+    "link": link
+  });
+};
+*/
+// When the window is fully loaded, call this function.
+// Note: because we are attaching an event listener to a particular HTML element
+// in this function, we can't do that until the HTML element in question has
+// been loaded. Otherwise, we're attaching our listener to nothing, and no code
+// will run when the submit button is clicked.
+$(window).load(function () {
+
+  // Find the HTML element with the id recommendationForm, and when the submit
+  // event is triggered on that element, call submitRecommendation.
+  $("#recommendationForm").submit(submitRecommendation);
+
+});
+
+// ugh yeh this is terrible js but it was one of my first js projects don't judge lol
